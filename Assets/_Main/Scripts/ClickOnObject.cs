590ee93dd58cc5edab68fbe53057d0192af13b98
Januar Elsan <januarelsan@gmail.com>
2019-10-18 11:31:14 +0700

    Create Zakat Calculator UI

---
+++ Assets/_Main/Scripts/ClickOnObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+public class ClickOnObject : MonoBehaviour
+{
+    public UnityEvent OnMouseDown_;
+    /// <summary>
+    /// OnMouseDown is called when the user has pressed the mouse button while
+    /// over the GUIElement or Collider.
+    /// </summary>
+    void OnMouseDown()
+    {
+        OnMouseDown_.Invoke();
+    }
+}
